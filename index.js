d96a5dc82806db1f8c1a83235a5365dc9c24145b
Nick Sette <njs2394@gmail.com>
2023-11-01 15:20:26 -0400

    more loops

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 
 
-function writeCards(name,event){
+function writeCards(name, event) {
     const messages = []
 
-    for(let i = 0; i < name.length; i++){
-    messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
+    for (let i = 0; i < name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`)
     }
     return messages
 
@@ -12,12 +12,43 @@ function writeCards(name,event){
 
 }
 
-function countDown(number){
+function countDown(number) {
     let i = 0
-    while(i <= number){
+    while (i <= number) {
         console.log(number - i)
         i++
     }
 
 }
 
+
+const eightHomersClub = [{
+    "name": "Randy Arozarena",
+    "total": 10,
+    "postseason": "2020"
+},
+{
+    "name": "Adolis Garcia",
+    "total": 8,
+    "postseason": "2023"
+},
+]
+
+function arroObjectLoop(arrOfObjects) {
+    for (let players in arrOfObjects) {
+        console.log(arrOfObjects[players]['name'])
+    }
+}
+
+arroObjectLoop(eightHomersClub)
+
+
+const yankees = ["Bath Ruth", 'Lou Gerhig', 'Mickey Mantle']
+
+function arrLoop(arr) {
+    for (let players of arr) {
+        console.log(players)
+    }
+}
+
+arrLoop(yankees)
